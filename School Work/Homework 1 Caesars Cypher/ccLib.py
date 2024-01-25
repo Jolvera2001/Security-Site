54b9217f09e9b62e8a539355c2c5f9fbf417253d
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-01-25 16:49:47 -0600

    Set up files and Dir

---
+++ School Work/Homework 1 Caesars Cypher/ccLib.py
@@ -0,0 +1,9 @@
+import string
+
+ALPHABET_LOWERCASE = list(string.ascii_lowercase)
+ALPHABET_UPPERCASE = list(string.ascii_uppercase)
+NUMBERS = list(string.digits)
+def encrypt(key):
+
+
+
---
+++ School Work/Homework 1 Caesars Cypher/main.py
@@ -0,0 +1,28 @@
+def main():
+    while True:
+        user_input = int(input("""
+        Select an option:
+        1. Encrypt Message
+        2. Decrypt Message
+        3. Break
+        4. Exit the program 
+        """))
+        if user_input == 1:
+            # Ask for Key value n
+            user_n_input = input("Enter your key N: ")
+
+        elif user_input == 2:
+            # Ask for Key value n
+            user_n_input = input("Enter your key N:")
+
+        elif user_input == 3:
+            # Break into encryption
+            print("Placeholder")
+
+        elif user_input == 4:
+            # Exit
+            break
+
+
+if __name__ == "__main__":
+    main()
