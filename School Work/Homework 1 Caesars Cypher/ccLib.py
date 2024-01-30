f2789c2b8aa77af452f0c784d94aa0cbd677f0a7
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-01-30 12:20:27 -0600

    Added Special char list

--- School Work/Homework 1 Caesars Cypher/ccLib.py
+++ School Work/Homework 1 Caesars Cypher/ccLib.py
@@ -2,6 +2,7 @@ import string
 
 ALPHABET_LOWERCASE = list(string.ascii_lowercase)
 ALPHABET_UPPERCASE = list(string.ascii_uppercase)
+SPECIAL_CHARS = list("\\.[]{}()<>*+-=!?^$|,' ")
 NUMBERS = list(string.digits)
 PLAIN_PATH = "./EDFiles/plaintext.txt"
 CIPHER_PATH = "./EDFiles/ciphertext.txt"
@@ -27,6 +28,10 @@ def encrypt(key):
         elif char.isdigit():
             char_index = NUMBERS.index(char)
             char = NUMBERS[(char_index + n) % 9]
+            cipherString += char
+        elif char in SPECIAL_CHARS:
+            char_index = SPECIAL_CHARS.index(char)
+            char = SPECIAL_CHARS[(char_index + n) % len(SPECIAL_CHARS)]
             cipherString += char
         else:
             cipherString += char
@@ -56,6 +61,10 @@ def decrypt(key):
         elif char.isdigit():
             char_index = NUMBERS.index(char)
             char = NUMBERS[(char_index - n) % 9]
+            plainString += char
+        elif char in SPECIAL_CHARS:
+            char_index = SPECIAL_CHARS.index(char)
+            char = SPECIAL_CHARS[(char_index - n) % len(SPECIAL_CHARS)]
             plainString += char
         else:
             plainString += char
