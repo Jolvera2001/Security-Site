b896a02504d0d26cbe8f11de1db97ca2e3925244
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-10 16:19:31 -0600

    Set up main file for use of library

--- School Work/HW2 OTP RC4/HW02-JohanOlvera.py
+++ School Work/HW2 OTP RC4/HW02-JohanOlvera.py
@@ -9,9 +9,18 @@ def main():
         2. RC4 Encryption
         3. Exit the program """))
         if user_input == 1:
-            print("OTP Ran")
+            # get their plaintext and key
+            user_pt_input = input("Please enter your plaintext")
+            user_key_input = input("Please enter your key (same length as plaintext)")
+            cipherText = OtpRc4Lib.Otp(user_pt_input, user_key_input)
+            print("Here is your Ciphertext: \n" + cipherText)
+
         elif user_input == 2:
-            print("RC4 Ran")
+            user_pt_input = input("Please enter your plaintext")
+            user_key_input = input("Please enter your key")
+            cipherText = OtpRc4Lib.Rc4(user_pt_input, user_key_input)
+            print("Here is your Ciphertext: \n" + cipherText)
+
         elif user_input == 3:
             print("goodbye!")
             break
