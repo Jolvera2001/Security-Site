28f1c96b236b589da4987f7448d91c43af1efc7c
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-11 15:15:20 -0600

    added unit test cases for future rc4 implementation

--- School Work/HW2 OTP RC4/OtpRc4Tests.py
+++ School Work/HW2 OTP RC4/OtpRc4Tests.py
@@ -20,5 +20,21 @@ class OtpUnitTests(unittest.TestCase):
         self.assertEqual(check2, string2)
         self.assertEqual(check3, string3)
 
+    def test_Rc4_basic(self):
+        string1 = "Hello World!"
+        key1 = "yellow borld"
+        string2 = "h3llo w0rld!"
+        key2 = "12345 67890!"
+        string3 = "th1s ' is weirdn nn"
+        key3 = "1234 ; we dfenfn nn"
+
+        throwaway1, check1 = OR.rc4(string1, key1)
+        throwaway2, check2 = OR.rc4(string2, key2)
+        throwaway3, check3 = OR.rc4(string3, key3)
+
+        self.assertEqual(check1, string1)
+        self.assertEqual(check2, string2)
+        self.assertEqual(check3, string3)
+
 if __name__ == "__main__":
     unittest.main()
