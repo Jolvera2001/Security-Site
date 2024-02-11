7078abd51df50ed3f100e9d0e30ffbae80ad6446
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-11 14:14:38 -0600

    Implemented otp

--- School Work/HW2 OTP RC4/HW02-JohanOlvera.py
+++ School Work/HW2 OTP RC4/HW02-JohanOlvera.py
@@ -12,14 +12,14 @@ def main():
             # get their plaintext and key
             user_pt_input = input("Please enter your plaintext")
             user_key_input = input("Please enter your key (same length as plaintext)")
-            ciphertext = OtpRc4Lib.otp(user_pt_input, user_key_input)
-            print("Here is your Ciphertext: \n" + ciphertext)
+            ciphertext, checked_text = OtpRc4Lib.otp(user_pt_input, user_key_input)
+            print("Here is your Ciphertext: \n" + ciphertext + "\n" + "Here is the original: \n" + checked_text)
 
         elif user_input == 2:
             user_pt_input = input("Please enter your plaintext")
             user_key_input = input("Please enter your key")
-            ciphertext = OtpRc4Lib.rc4(user_pt_input, user_key_input)
-            print("Here is your Ciphertext: \n" + ciphertext)
+            ciphertext, checked_text = OtpRc4Lib.rc4(user_pt_input, user_key_input)
+            print("Here is your Ciphertext: \n" + ciphertext + "\n" + "Here is the original: \n" + checked_text)
 
         elif user_input == 3:
             print("goodbye!")
--- School Work/HW2 OTP RC4/OtpRc4Lib.py
+++ School Work/HW2 OTP RC4/OtpRc4Lib.py
@@ -1,11 +1,35 @@
 def otp(pt, key):
-    print(pt, key)
+    if len(key) != len(pt):
+        return "Pt and Key are not the same size!"
+
+    # doing the xor for each character
+    cipher_text = ""
+    for i in range(len(pt)):
+        pt_char = ord(pt[i])
+        key_char = ord(key[i])
+        cipher_text += chr(pt_char ^ key_char)
+
+    checked_text = ""
+    for i in range(len(cipher_text)):
+        cipher_char = ord(cipher_text[i])
+        key_char = ord(key[i])
+        checked_text += chr(cipher_char ^ key_char)
+
+    if checked_text == pt:
+        return cipher_text, checked_text
+    else:
+        # debugging purposes
+        print("Something went wrong")
+        return "None", "None"
+
 
 def rc4(pt, key):
     '''
     We need to use otp at the end when the
-    shortkey is lengthended to the size of the pt
+    shortkey is expanded to the size of the pt
     1. Extend key
     2. otp with extended key
     '''
-    print(pt, key)
+    # Initialization
+    test = 100 ^ 500
+    # KBG
