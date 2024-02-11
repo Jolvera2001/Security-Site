9d62a4fb3157d4e98c7910e047ae14aa9dba2681
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-10 19:03:17 -0600

    added comment in rc4

--- School Work/HW2 OTP RC4/OtpRc4Lib.py
+++ School Work/HW2 OTP RC4/OtpRc4Lib.py
@@ -2,4 +2,10 @@ def otp(pt, key):
     print(pt, key)
 
 def rc4(pt, key):
+    '''
+    We need to use otp at the end when the
+    shortkey is lengthended to the size of the pt
+    1. Extend key
+    2. otp with extended key
+    '''
     print(pt, key)
